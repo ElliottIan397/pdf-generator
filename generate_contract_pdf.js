7ac1c4493168a0fdc07793dffe483d982c4bdf14
Ian Elliott <ianelliott@eandssolutions.com>
2025-07-07 18:19:57 -0500

    📄 Add bias breakdown columns to Devices table in contract

--- generate_contract_pdf.js
+++ generate_contract_pdf.js
@@ -28,17 +28,14 @@ data.Devices_Table = data.Devices_Table.map(device => ({
 // Sort devices by annual volume (descending)
 data.Devices_Table = data.Devices_Table
   .sort((a, b) => b.Volume - a.Volume)
-  .map(device => `
-    <tr>
-      <td>${device.Model}</td>
-      <td>${device.Serial}</td>
-      <td>${device.Volume.toLocaleString()}</td>
-      <td>${device.Black_Bias || "N/A"}</td>
-      <td>${device.Cyan_Bias || "N/A"}</td>
-      <td>${device.Magenta_Bias || "N/A"}</td>
-      <td>${device.Yellow_Bias || "N/A"}</td>
-    </tr>
-  `).join('');
+  .map(device => ({
+    ...device,
+    Volume: device.Volume.toLocaleString(),
+    Black_Bias: device[`Black_Bias_${biasKey}`] || "N/A",
+    Cyan_Bias: device[`Cyan_Bias_${biasKey}`] || "N/A",
+    Magenta_Bias: device[`Magenta_Bias_${biasKey}`] || "N/A",
+    Yellow_Bias: device[`Yellow_Bias_${biasKey}`] || "N/A",
+  }));
 
 const guardrails = [
   ["Fleet Output Avg. Mth. Lower Limit:", data.volumeLowerLimit],
