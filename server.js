2d1869a0d625d24f1b37648150c8fa1407c9c600
Ian Elliott <ianelliott@eandssolutions.com>
2025-07-09 01:10:46 -0500

    Fix error in server.js file

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ app.post("/generate-pdf", async (req, res) => {
 });
 
 const { getAccessToken } = require("./docusignClient");
-const fs = require("fs");
 const path = require("path");
 
 app.post("/send-envelope", async (req, res) => {
