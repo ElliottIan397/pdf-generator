a7ad4a6bcc3a9df040874adfc3b89145c8379754
Ian Elliott <ianelliott@eandssolutions.com>
2025-07-10 15:52:13 -0500

    address CORS

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(bodyParser.json({ limit: "2mb" }));
 
 app.post("/generate-pdf", async (req, res) => {
